32b9d9a778b4c6ba579a23ab451418bdd7385fbe
Iespinoza <italoespinoza1529@gmail.com>
2024-06-09 00:10:09 -0500

    -

--- examen/examen/src/main/java/com/laboratorio/examen/repository/IUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laboratorio.examen.repository;
-
-import com.laboratorio.examen.model.UsuarioModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUsuarioRepository extends JpaRepository<UsuarioModel, Integer> {
-}
